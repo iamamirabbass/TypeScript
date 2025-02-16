d538998b9ce618716dae983d652fb250b7521204
Amir Abbass <amirabbass334@gmail.com>
2025-02-16 16:29:46 +0500

    2questions complete

--- Typescript#01/dist/app.js
+++ Typescript#01/dist/app.js
@@ -1,5 +1,14 @@
 "use strict";
-user;
-{
+// *********************************************************** QNO1 ***********************************************************
+function createUser(user) {
+    return user;
 }
+const newUser = createUser({
+    id: 123,
+    name: 'amir',
+    email: 'amirabbass334@gmail.com',
+    active: true
+});
+console.log(newUser);
+// *********************************************************** QNO2 ***********************************************************
 //# sourceMappingURL=app.js.map
--- Typescript#01/src/app.ts
+++ Typescript#01/src/app.ts
@@ -1,4 +1,5 @@
 // *********************************************************** QNO1 ***********************************************************
+
 interface User{
     id:number;
     name:string;
@@ -18,4 +19,18 @@ const newUser=createUser(
         active:true
     })
 console.log(newUser);
+
+
 // *********************************************************** QNO2 ***********************************************************
+
+type Input = number | string;
+
+function processInput(input: Input): string {
+    if (typeof input === "number") {
+        return `Number: ${input.toString()}`;
+    } else {
+        return `String: ${input}`;
+    }
+}
+console.log(processInput(42));       
+console.log(processInput("Hello"));  
